0bc11e5e2670db538da6e31689f11fd4eb3265f1
benjaminpettus <benjaminpettus@gmail.com>
2017-05-01 23:25:24 -0700

    server running, home route set-up

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require( 'express' )
 const app = express()
-const bodyParser = require('body-parser')
-const logger = require('morgan')
+const logger = require( 'morgan' )
+const path = require( 'path' )
+const bodyParser = require( 'body-parser' )
+const pug = require('pug')
 const PORT = 3000
 
+app.use(logger( 'dev' ) )
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( express.static( path.join( __dirname, 'public' ) ) )
+
+app.set( 'views', path.join(__dirname, 'views' ) )
+app.set( 'view engine', 'pug' )
 
 
+app.get('/', ( req, res ) => {
+  res.render('index')
+})
 
 
 
